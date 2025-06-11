75d10ca234e23c3be9e6f21e5f9572e8dd7504c3
Alexandre Duteil <alexandre.duteil@gmail.com>
2025-06-11 17:59:12 +0200

    usercollection controller

--- app/controllers/user_collections_controller.rb
+++ app/controllers/user_collections_controller.rb
@@ -1,22 +1,80 @@
 class UserCollectionsController < ApplicationController
-	def index
-	end
+  before_action :authenticate_user!
+  before_action :set_user_collection, only: [:show, :edit, :update, :destroy]
 
-	def show
-	end
+  def index
+    @usercollections = current_user.user_collections
+    @owned_mangas = current_user.owned_mangas
+    @db_mangas = DbManga.all
+  end
 
-	def new
-	end
+  def show
+    @owned_mangas = @user_collection.owned_mangas
+    @db_mangas = DbManga.all
 
-	def create
-	end
+    respond_to do |format|
+      format.html
+      format.json { render json: @user_collection }
+    end
+  end
 
-	def edit
-	end
+  def new
+    @user_collection = current_user.user_collections.build
+    @owned_mangas = current_user.owned_mangas # Seulement les mangas de l'user
+    @db_mangas = DbManga.all
 
-	def update
-	end
+    respond_to do |format|
+      format.html
+      format.json { render json: @user_collection }
+    end
+  end
 
-	def destroy
-	end
+  def create
+    @user_collection = current_user.user_collections.build(user_collection_params)
+
+    if @user_collection.save
+      redirect_to @user_collection, notice: 'Collection créée avec succès.'
+    else
+      @owned_mangas = current_user.owned_mangas
+      @db_mangas = DbManga.all
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @owned_mangas = @user_collection.owned_mangas
+    @db_mangas = DbManga.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @user_collection }
+    end
+  end
+
+  def update
+    if @user_collection.update(user_collection_params)
+      redirect_to @user_collection, notice: 'Collection mise à jour avec succès.'
+    else
+      @owned_mangas = @user_collection.owned_mangas
+      @db_mangas = DbManga.all
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user_collection.destroy
+    redirect_to user_collections_path, notice: 'Collection supprimée avec succès.'
+  end
+
+  private
+
+  def set_user_collection
+    @user_collection = current_user.user_collections.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to user_collections_path, alert: 'Collection introuvable.'
+  end
+
+  def user_collection_params
+    params.require(:user_collection).permit(:name)
+  end
 end
--- app/models/user_collection.rb
+++ app/models/user_collection.rb
@@ -1,4 +1,4 @@
 class UserCollection < ApplicationRecord
   belongs_to :user, dependent: :destroy
-  has_many :owned_manga
+  has_many :owned_mangas, dependent: :destroy
 end
