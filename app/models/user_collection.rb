8de18ce09bec3ea20e2889d630cd55164f375e72
Alexandre Duteil <alexandre.duteil@gmail.com>
2025-06-12 22:54:32 +0200

    validates user collection

--- app/models/user_collection.rb
+++ app/models/user_collection.rb
@@ -1,4 +1,6 @@
 class UserCollection < ApplicationRecord
   belongs_to :user
   has_many :owned_mangas, dependent: :destroy
+
+  validates :name, presence: true, length: { minimum: 3, maximum: 50 }
 end
