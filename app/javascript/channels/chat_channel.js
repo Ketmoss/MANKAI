5f6d70f62d09c8122528ede33da75c9c8af44ea7
Alexandre Duteil <alexandre.duteil@gmail.com>
2025-06-16 12:22:36 +0200

    turbo

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -1,12 +1,24 @@
 import consumer from "./consumer"
 
-document.addEventListener('DOMContentLoaded', () => {
-  const chatElement = document.getElementById('chat-messages');
+let subscription;
+
+document.addEventListener("turbo:load", () => {
+  const chatElement = document.getElementById("chat-messages");
+  if (!chatElement) return;
+
   const chatId = chatElement.dataset.chatId;
 
-  consumer.subscriptions.create({ channel: "ChatChannel", chat_id: chatId }, {
-    received(data) {
-      chatElement.insertAdjacentHTML('beforeend', data.message);
+  if (subscription) {
+    consumer.subscriptions.remove(subscription);
+  }
+
+  subscription = consumer.subscriptions.create(
+    { channel: "ChatChannel", chat_id: chatId },
+    {
+      received(data) {
+        chatElement.insertAdjacentHTML("beforeend", data.message);
+        chatElement.scrollTop = chatElement.scrollHeight; // scroll vers le bas
+      }
     }
-  });
+  );
 });
