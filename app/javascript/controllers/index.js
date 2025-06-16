f359b1760d455f8f6c6e163618e4c4917176d4c0
Alexandre Duteil <alexandre.duteil@gmail.com>
2025-06-16 18:04:40 +0200

    repair

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -2,7 +2,3 @@
 import { application } from "controllers/application"
 import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 eagerLoadControllersFrom("controllers", application)
-
-
-import ChatMessageController from "./chat_message_controller"
-application.register("chat-message", ChatMessageController)
