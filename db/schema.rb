def086d03b4d233aa5419d4a254e26ab81accf8e
Alexandre Duteil <alexandre.duteil@gmail.com>
2025-06-19 19:43:42 +0200

    maj finale

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema[7.1].define(version: 2025_06_18_200818) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_trgm"
@@ -50,7 +48,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_18_200818) do
     t.integer "jikan_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["jikan_id"], name: "index_db_mangas_on_jikan_id", unique: true
   end
 
   create_table "exchanges", force: :cascade do |t|
@@ -102,7 +99,6 @@ ActiveRecord::Schema[7.1].define(version: 2025_06_18_200818) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id", null: false
-    t.boolean "read"
     t.index ["chat_id"], name: "index_messages_on_chat_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
